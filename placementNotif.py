a6a1240f4c58b619243f6dd425df20966527f3b3
Alankar Kotwal <alankarkotwal13@gmail.com>
2015-12-01 16:43:10 +0530

    support multiple attachments in placementNotif

--- placementNotif.py
+++ placementNotif.py
@@ -1,7 +1,6 @@
 #################################################
 # Placement Notification Script '15             #
 # Alankar Kotwal <alankarkotwal13@gmail.com>    #
-# Email attachment part still untested          #
 #################################################
 
 import urllib2, base64, getpass, smtplib, pdb
@@ -24,66 +23,69 @@ me = "aloo@wncc-iitb.org"
 you = "alankarkotwal13+placements@gmail.com"
 
 while 1:
-
-	req = urllib2.Request(theurl)
-	req.add_header("Authorization", authheader)
-
-	handle = urllib2.urlopen(req)
-	resp = handle.read()
-
-	soup = bs(resp, "html.parser")
-	mydivs = soup.findAll("div", { "class" : "post" })
-	
-	now = mydivs[0].findAll('a')[0]
-	nowString = now.string
-
-	if last != nowString:
-
-		link = now['href'] 
-		
-		reqIn = urllib2.Request(link)
-		reqIn.add_header("Authorization", authheader)
-
-		handleIn = urllib2.urlopen(reqIn)
-		respIn = handleIn.read()
-		
-		soupIn = bs(respIn, "html.parser")
-		mydivsIn = soupIn.findAll("div", { "class" : "entry" })
-
-		attLink = mydivsIn[0].findAll('a')[0]['href']
-
-		msg = MIMEMultipart('alternative')
-		msg['Subject'] = nowString
-		msg['From'] = me
-		msg['To'] = you
-		
-		text = link
-		html = respIn
-
-		part1 = MIMEText(text, 'plain')
-		part2 = MIMEText(html, 'html')
-
-		msg.attach(part1)
-        	msg.attach(part2)
-
-		if ".xlsx" in attLink or ".csv" in attLink or ".docx" in attLink:
-			
-			attName = attLink.split('/')[-1]
-
-			attReq = urllib2.Request(attLink)
-			attReq.add_header("Authorization", authheader)
-			
-			attHandle = urllib2.urlopen(attReq)
-			attachment = attHandle.read()
-
-			part3 = MIMEBase('application', "octet-stream")
-			part3.set_payload(attachment)
-			encoders.encode_base64(part3)
-			part3.add_header('Content-Disposition', 'attachment; filename="'+attName+'"')
-			msg.attach(part3)
-		
-		s = smtplib.SMTP('localhost')
-		s.sendmail(me, [you], msg.as_string())
-		s.quit()
-
-		last = nowString
+    
+    req = urllib2.Request(theurl)
+        req.add_header("Authorization", authheader)
+        
+        handle = urllib2.urlopen(req)
+        resp = handle.read()
+        
+        soup = bs(resp, "html.parser")
+        mydivs = soup.findAll("div", { "class" : "post" })
+        
+        now = mydivs[0].findAll('a')[0]
+        nowString = now.string
+        
+        if last != nowString:
+            
+            link = now['href']
+                
+                reqIn = urllib2.Request(link)
+                reqIn.add_header("Authorization", authheader)
+                
+                handleIn = urllib2.urlopen(reqIn)
+                respIn = handleIn.read()
+                
+                soupIn = bs(respIn, "html.parser")
+                mydivsIn = soupIn.findAll("div", { "class" : "entry" })
+                
+                attLinkTags = mydivsIn[0].findAll('a')
+                
+                msg = MIMEMultipart('alternative')
+                msg['Subject'] = nowString
+                msg['From'] = me
+                msg['To'] = you
+                
+                text = link
+                html = respIn
+                
+                part1 = MIMEText(text, 'plain')
+                part2 = MIMEText(html, 'html')
+                
+                msg.attach(part1)
+                msg.attach(part2)
+                
+                for tag in attLinkTags:
+                    
+                    attLink = tag['href']
+                        if ".xlsx" in attLink or ".csv" in attLink or ".docx" in attLink:
+                            
+                            attName = attLink.split('/')[-1]
+                                
+                                attReq = urllib2.Request(attLink)
+                                attReq.add_header("Authorization", authheader)
+                                
+                                attHandle = urllib2.urlopen(attReq)
+                                attachment = attHandle.read()
+                                
+                                part3 = MIMEBase('application', "octet-stream")
+                                part3.set_payload(attachment)
+                                encoders.encode_base64(part3)
+                                part3.add_header('Content-Disposition', 'attachment; filename="'+attName+'"')
+                                msg.attach(part3)
+                
+                s = smtplib.SMTP('localhost')
+                s.sendmail(me, [you], msg.as_string())
+                s.quit()
+                
+            last = nowString
