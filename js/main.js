919bd1fb73413cf88896c49e48ff726f5f3d315b
Alankar Kotwal <alankarkotwal13@gmail.com>
2016-06-02 21:59:26 +0530

    Swap webpages, bring sandbox to front

--- js/main.js
+++ js/main.js
@@ -1,35 +1,122 @@
-(function() {
-  $(document).ready(function() {
-    var animate, item, items, mobile, _i, _len;
-    $(".nav ul li a").click(function() {
-      return $('html, body').animate({
-        scrollTop: $("#" + ($(this).attr('class'))).offset().top
-      }, 1500);
-    });
-    $(".chevron .down").click(function() {
-      return $('html, body').animate({
-        scrollTop: $("#about").offset().top
-      }, 1500);
-    });
-    mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-    if ($("html").hasClass("cssanimations") && !mobile) {
-      animate = function(animation) {
-        return $(animation[0]).addClass("concealed").viewportChecker({
-          classToAdd: "visible animated " + (animation[1].trim()),
-          offset: 200
-        });
-      };
-      items = [['#about p', 'fadeInUp'], ['.column', 'fadeInUp'], ['.item', 'fadeInDown'], ['.bar', 'fadeInRight'], ['.paperplane', 'bounceInDown'], ['#quote .container', 'fadeIn'], ['#quote .icon', 'fadeInDown']];
-      for (_i = 0, _len = items.length; _i < _len; _i++) {
-        item = items[_i];
-        animate(item);
-      }
-    }
-    return $("#footer ul li").hover(function() {
-      return $(this).find('p').stop(true, true).fadeIn(800);
-    }, function() {
-      return $(this).find('p').stop(true, true).fadeOut(800);
-    });
-  });
+/* ========================================================================= */
+/*  Preloader Script
+/* =========================================================================
 
-}).call(this);
+window.onload = function () {
+    document.getElementById('loading-mask').style.display = 'none';
+} */
+
+$(function(){
+    /* ========================================================================= */
+    /*  Menu item highlighting
+    /* ========================================================================= */
+
+    jQuery(window).scroll(function () {
+        if (jQuery(window).scrollTop() > 400) {
+            jQuery("#navigation").css("background-color","#00C7FC");
+            jQuery("#navigation").addClass("animated-nav");
+        } else {
+            jQuery("#navigation").css("background-color","transparent");
+            jQuery("#navigation").removeClass("animated-nav");
+        }
+    });
+
+    $('#nav').onePageNav({
+        filter: ':not(.external)',
+        scrollSpeed: 950,
+        scrollThreshold: 1
+    });
+
+    // Slider Height
+    var slideHeight = $(window).height();
+    $('#home-carousel .carousel-inner .item, #home-carousel .video-container').css('height',slideHeight);
+
+    $(window).resize(function(){'use strict',
+        $('#home-carousel .carousel-inner .item, #home-carousel .video-container').css('height',slideHeight);
+    });
+
+    // portfolio filtering
+
+    $("#projects").mixItUp();
+
+    //fancybox
+
+    $(".fancybox").fancybox({
+        padding: 0,
+
+        openEffect : 'elastic',
+        openSpeed  : 650,
+
+        closeEffect : 'elastic',
+        closeSpeed  : 550,
+    });
+
+
+    /* ========================================================================= */
+    /*  Facts count
+    /* ========================================================================= */
+
+    "use strict";
+    $(".fact-item").appear(function () {
+        $(".fact-item [data-to]").each(function () {
+            var e = $(this).attr("data-to");
+            $(this).delay(6e3).countTo({
+                from: 50,
+                to: e,
+                speed: 3e3,
+                refreshInterval: 50
+            })
+        })
+    });
+
+/* ========================================================================= */
+/*  On scroll fade/bounce fffect
+/* ========================================================================= */
+
+    $("#testimonial").owlCarousel({
+        pagination : true, // Show bullet pagination
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
+    });
+
+});
+
+/* ========================================================================= */
+/*  On scroll fade/bounce fffect
+/* ========================================================================= */
+
+    wow = new WOW({
+        animateClass: 'animated',
+        offset: 100,
+        mobile: false
+    });
+    wow.init();
+
+/* ---------------------------------------------------------------------- */
+/*      Progress Bars
+/* ---------------------------------------------------------------------- */
+
+initProgress('.progress');
+
+function initProgress(el){
+    jQuery(el).each(function(){
+        var pData = jQuery(this).data('progress');
+        progress(pData,jQuery(this));
+    });
+}
+
+
+            
+function progress(percent, $element) {
+    var progressBarWidth = 0;
+    
+    (function myLoop (i,max) {
+        progressBarWidth = i * $element.width() / 100;
+        setTimeout(function () {   
+        $element.find('div').find('small').html(i+'%');
+        $element.find('div').width(progressBarWidth);
+        if (++i<=max) myLoop(i,max);     
+        }, 10)
+    })(0,percent);  
+}   
